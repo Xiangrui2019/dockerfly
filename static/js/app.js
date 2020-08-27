81eef0fb5c41d01abdda8292249f8495a6fe57c6
Xiangrui2019 <xiangrui@aiursoft.com>
2020-08-27 09:01:15 +0800

    homepage

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,32 @@
-'use strict';
-
-var app = new Vue({
-    el: '#app',
-    data: {
-        "homePageDisplay": true,
-        "sitePageDisplay": false,
-        "servicePageDisplay": false,
-        "logPageDisplay": false,
-        "helpPageDisplay": false,
+var dockerfly = {
+    homeDisplay: function () {
+        this.hiddenAll();
+        $(".home").css({"display": "inline"});
+    },
+    siteDisplay: function () {
+        this.hiddenAll();
+        $(".site").css({"display": "inline"});
+    },
+    serviceDisplay: function () {
+        this.hiddenAll();
+        $(".service").css({"display": "inline"});
+    },
+    logDisplay: function () {
+        this.hiddenAll();
+        $(".log").css({"display": "inline"});
+    },
+    helpDisplay: function () {
+        this.hiddenAll();
+        $(".help").css({"display": "inline"});
+    },
+    hiddenAll: function () {
+        $(".home").css({"display": "none"});
+        $(".site").css({"display": "none"});
+        $(".service").css({"display": "none"});
+        $(".log").css({"display": "none"});
+        $(".help").css({"display": "none"});
+    },
+    blocks: {
+        home: homePage
     }
-});
+};
